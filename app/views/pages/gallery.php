d71557aca45a92b93ec5e74500245007395275c6
Vladimir Basaric <vladimirbasaric@gmail.com>
2019-10-08 22:41:49 +0200

    sredjena galerija, lightbox, responsive,  samo treba ubaciti original slike

--- app/views/pages/gallery.php
+++ app/views/pages/gallery.php
@@ -1,20 +1,72 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
   <h1><?php echo $data['title']; ?></h1>
-  <img src='../img/dance.jpg' alt='no image'> 
+  <!-- putanja -->
+  <!-- <img src='../img/dance.jpg' alt='no image'>  -->
 
-<div class="gallery">
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
-  <a href="../img/slika.jpg" data-lightbox="gallery1"><img src="../img/slika_mala.jpg" alt="slika"></a>
+<div class="container gallery">
+  <hr class="mt-2 mb-5">
+  <div class="row text-center text-lg-left">
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div>
+    <div class="col-lg-3 col-md-4 col-6">
+      <a href="../img/slika.jpg" class="d-block mb-4 h-100" data-lightbox="gallery1">
+        <img class="img-fluid img-thumbnail" src="../img/slika_mala.jpg" alt="slika">
+      </a>
+    </div> 
+  </div>
 </div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
